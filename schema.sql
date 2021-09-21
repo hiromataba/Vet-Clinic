1a6cf5b9533c12b784b464fbed7c610146868a18
Hiro Mataba <aganzemataba@gmail.com>
2021-09-21 10:20:47 +0200

    Update schema

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INT PRIMARY KEY,
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR(100),
     date_of_birth DATE,
     escape_attempts INT NOT NULL,
