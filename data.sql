5b737d6ff8ada500e50329dff45063140b1a5747
Hiro Mataba <aganzemataba@gmail.com>
2021-09-21 10:21:05 +0200

    Update INSERT date

--- data.sql
+++ data.sql
@@ -1,4 +1,11 @@
 /* Populate database with sample data. */
 
 INSERT INTO animals (name,date_of_birth,weight_kg,neutered,escape_attempts)
-VALUES ('Agumon','Feb 3rd, 2020',10.23,TRUE,0);
+VALUES ('Agumon','Feb 3, 2020',10.23,TRUE,0);
+INSERT INTO animals (name,date_of_birth,weight_kg,neutered,escape_attempts)
+VALUES ('Gabumon','Nov 15, 2018',8,TRUE,2);
+INSERT INTO animals (name,date_of_birth,weight_kg,neutered,escape_attempts)
+VALUES ('Pikachu','Jan 7, 2021',15.04,FALSE,1);
+INSERT INTO animals (name,date_of_birth,weight_kg,neutered,escape_attempts)
+VALUES ('Devimon','May 12, 2017',11,TRUE,5);
+
