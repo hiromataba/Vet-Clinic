39f455ea7c7639c6ae5b7de50b5be0e7a9f34a0e
Hiro Mataba <aganzemataba@gmail.com>
2021-09-21 12:52:51 +0200

    Update Required changes

--- queries.sql
+++ queries.sql
@@ -2,7 +2,7 @@
 
 SELECT * FROM animals WHERE name LIKE '%mon';
 SELECT * FROM animals WHERE date_of_birth BETWEEN  '2016-01-01' AND '2019-12-31';
-SELECT name FROM animals WHERE escape_attempts < 3;
+SELECT name FROM animals WHERE escape_attempts < 3 AND neutered = TRUE;
 SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = TRUE;
