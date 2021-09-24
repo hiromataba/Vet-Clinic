23675d5549b37a7587368ae5fa4825ccf8ed6965
Hiro Mataba <aganzemataba@gmail.com>
2021-09-24 08:33:43 +0200

    Create Vets table

--- schema.sql
+++ schema.sql
@@ -31,3 +31,13 @@ ALTER TABLE animals ADD species_id INT;
 ALTER TABLE animals ADD owner_id INT; 
 ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species(id); 
 ALTER TABLE animals ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+/* Add "join table" for visits */
+
+CREATE TABLE vets (
+    id                  INT GENERATED ALWAYS AS IDENTITY,
+    name                VARCHAR(100),
+    age                 INT,
+    date_of_graduation  DATE,
+    PRIMARY KEY(id)
+);
